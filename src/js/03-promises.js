b2795bc0017ead2b6fabf16be60ec1e107adc0ff
annapaniv <129009432+annapaniv@users.noreply.github.com>
2023-08-21 16:27:46 +0200

    promice

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+const { delay, step, amount } = form.elements;
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+
+  let firstDelay = Number(delay.value);
+  let stepTime = Number(step.value);
+  let count = Number(delay.value);
+
+
+  for (let i = 1; i <= Number(amount.value); i += 1) {
+    setTimeout(() => {
+
+      let promise = createPromise(i, (count));
+      promise.then((value) => { Notify.success(value)})
+        .catch((error) => { Notify.failure(error) });
+      count += stepTime;
+
+    }, firstDelay);
+    firstDelay += stepTime;
+  };
+  evt.preventDefault();
+
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
+
   if (shouldResolve) {
-    // Fulfill
+    return new Promise((resolve, reject) => {
+      resolve(`Fulfilled promise ${position} in ${delay}`)
+    })
   } else {
-    // Reject
+    return new Promise((resolve, reject) => {
+      reject(`Rejected promise ${position} in ${delay}`)
+    })
   }
 }
